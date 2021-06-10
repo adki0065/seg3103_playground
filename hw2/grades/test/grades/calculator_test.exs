7dd710af91fff39cd9fc0f38b2e3d9c02548a193
John Adkin <jadki065@uottawa.ca>
2021-06-09 22:16:11 -0400

    Merge branch 'master' of https://github.com/adki0065/seg3103_playground updated date

---
+++ hw2/grades/test/grades/calculator_test.exs
@@ -0,0 +1,18 @@
+ExUnit.start()
+
+defmodule Grades.CalculatorTest do
+  use ExUnit.Case
+  alias Grades.Calculator
+
+  describe "percentage_grade/1" do
+    test "sample" do
+      assert 85 ==
+               Calculator.percentage_grade(%{
+                 homework: [0.8],
+                 labs: [1, 1, 1],
+                 midterm: 0.70,
+                 final: 0.9
+               })
+    end
+  end
+end
