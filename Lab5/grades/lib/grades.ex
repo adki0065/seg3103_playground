ad53323a9dc939b2fdad643f51d877521468bbc5
John Adkin <jadki065@uottawa.ca>
2021-07-01 12:24:21 -0400

    lab5 with stubs

--- Lab5/grades/lib/grades.ex
+++ Lab5/grades/lib/grades.ex
@@ -2,5 +2,18 @@ defmodule Grades do
 
   defmodule Calculator do
 
+  def letter_grade(marks) do
+    Enum.random(['A','B', 'C', 'D', 'EIN', 'F'])
+  end
+
+  def numeric_grade(marks) do
+    :rand.uniform(10)
+  end
+
+  def percentage_grade(marks) do
+    :rand.uniform(100)
+  end
+
+end
 
 end
